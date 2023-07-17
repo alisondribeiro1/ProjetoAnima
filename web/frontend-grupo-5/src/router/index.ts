2ee0efa8255b9c87f89815a34dc0d228047a8017
Eduardo Moraes Goncalves <eduardo.m.goncalves@unicuritiba.com.br>
2023-07-17 13:54:26 -0300

    Adição do frontend

---
+++ web/frontend-grupo-5/src/router/index.ts
@@ -0,0 +1,42 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/HomeView.vue"),
+  },
+  {
+    path: "/inscricao",
+    name: "Inscricao",
+    component: () => import("@/views/InscricaoView.vue"),
+  },
+  {
+    path: "/certificados",
+    name: "Certificados",
+    component: () => import("@/views/CertificadosView.vue"),
+  },
+  {
+    path: "/boletos",
+    name: "Boletos",
+    component: () => import("@/views/BoletosView.vue"),
+  },
+  {
+    path: "/notas",
+    name: "Notas",
+    component: () => import("@/views/NotasView.vue"),
+  },
+  {
+    path: "/cursos",
+    name: "Cursos",
+    component: () => import("@/views/CursosView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
