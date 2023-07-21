2c12319ebee7cd6d7b402481252352e89d445f93
Alison Ribeiro <alison.ribeiro@animaeducacao.com.br>
2023-07-21 11:40:48 -0300

    Implementação da fila de mensagens Kafka

--- src/Usuario/Usuario.API/Program.cs
+++ src/Usuario/Usuario.API/Program.cs
@@ -1,57 +1,19 @@
-using Usuario.Infrastructure.Data;
-using Usuario.Infrastructure.Repositories;
-using Usuario.Infrastructure.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
+using Usuario.API;
 
-namespace Usuario.API
+namespace Alunos.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var connectionString = configuration.GetConnectionString("PostgreSQLConnection");
-
-            builder.Services.AddDbContext<UsuarioDbContext>(options =>
-                options.UseNpgsql(connectionString, b => b.MigrationsAssembly("Usuario.API")));
-
-            builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-          
-
-            var app = builder.Build();
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-            //  Configure the HTTP request pipeline.
-            //   if (app.Environment.IsDevelopment())
-            // {
-            app.UseSwagger();
-                app.UseSwaggerUI();
-
-            //    app.UseHttpsRedirection();
-           // }
-
-            
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
