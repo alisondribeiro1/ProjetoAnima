c086ac361fad06689fe5b343fcf622a4e234b3ca
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2023-07-26 09:53:26 -0300

    Habilitando o swagger.

--- src/Curso/Curso.Api/Program.cs
+++ src/Curso/Curso.Api/Program.cs
@@ -35,12 +35,9 @@ namespace Curso.Api
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+          
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
